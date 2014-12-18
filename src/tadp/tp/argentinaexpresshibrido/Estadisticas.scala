e6c50472e1bb28e6a5df374ad76f5fb1f15dcb74
FranciscoPinieiro <panchopr_91@hotmail.com>
2014-12-18 18:37:34 -0300

    Agrego uso de filtros genericos en estadisticas. Falta definir aplicacion parical y testear.

--- src/tadp/tp/argentinaexpresshibrido/Estadisticas.scala
+++ src/tadp/tp/argentinaexpresshibrido/Estadisticas.scala
@@ -3,7 +3,7 @@ package tadp.tp.argentinaexpresshibrido
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
 
-class Tupla (nombre:String, cantidad:Int){
+class Tupla (nombre:String, cantidad:Unit){
 }
 
 class Estadisticas extends prettyPrinter{
@@ -65,10 +65,12 @@ class Estadisticas extends prettyPrinter{
     viajes.toList.map(_.ganancia).sum
   }
   
-  def impresionEstadisticasPorTransporte(viajes: Set[Viaje], f1: (Set[Viaje], String) => Set[Viaje], f2: Set[Viaje] => _, titulo: String, subt: String) ={
-    val viajesCamion : Set[Viaje] = f1(viajes, "Camion")
-    val viajesFurgoneta : Set[Viaje] = f1(viajes, "Furgoneta")
-    val viajesAvion : Set[Viaje] = f1(viajes, "Avion")
+  // Comparacion entre distintos tipos de Transporte
+  def EstadisticasPorTransporte(viajes: Set[Viaje], f2: Set[Viaje] => _, titulo: String, subt: String) ={
+    
+    val viajesCamion : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Camion")
+    val viajesFurgoneta : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Furgoneta")
+    val viajesAvion : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Avion")
     
     println(titulo)
     println("--------------------------")
@@ -77,31 +79,65 @@ class Estadisticas extends prettyPrinter{
     println("Furgoneta    :    " + f2(viajesFurgoneta))
     println("Avion        :    " + f2(viajesAvion))
     println("--------------------------\n")
-   // val tuplaCamion=new Tupla("camion", f2(viajesCamion))
-    //val tuplaFurgoneta=new Tupla("furgoneta", f2(viajesFurgoneta))
-    //val tuplaAvion=new Tupla("avion", f2(viajesAvion))
-    //val datos:Set[Tupla]=Set()
-    //datos+tuplaCamion+ tuplaFurgoneta+tuplaAvion
+    val tuplaCamion: Tupla=new Tupla("camion", f2(viajesCamion))
+    val tuplaFurgoneta: Tupla=new Tupla("furgoneta", f2(viajesFurgoneta))
+    val tuplaAvion: Tupla=new Tupla("avion", f2(viajesAvion))
+    var datos:Set[Tupla]=Set()
+    //datos += tuplaCamion + tuplaFurgoneta + tuplaAvion
    }
   
-  //Funciones con orden superior
+  //Ejemplos de distintos filtros posibles
   def enviosPorTipoTransporte(viajes: Set[Viaje]) = {
-    impresionEstadisticasPorTransporte(viajes, filtrarPorTipoDeTransporte,cantidadDeEnvios,"ENVIOS POR TRANSPORTE","Cantidad")
+    EstadisticasPorTransporte(viajes,cantidadDeEnvios,"ENVIOS POR TRANSPORTE","Cantidad")
   }
   
   def tiempoPromedioPorTransporte(viajes: Set[Viaje]) = {
-    impresionEstadisticasPorTransporte(viajes, filtrarPorTipoDeTransporte,tiempoPromedioDeViajes,"TIEMPO PROMEDIO DE VIAJE","Tiempo(Hr)")
+    EstadisticasPorTransporte(viajes,tiempoPromedioDeViajes,"TIEMPO PROMEDIO DE VIAJE","Tiempo(Hr)")
   }
   
    def costoPromedioPorTransporte(viajes: Set[Viaje]) = {
-    impresionEstadisticasPorTransporte(viajes, filtrarPorTipoDeTransporte,costoPromedioDeViajes,"COSTO PROMEDIO DE VIAJE","Costo($)")
+    EstadisticasPorTransporte(viajes,costoPromedioDeViajes,"COSTO PROMEDIO DE VIAJE","Costo($)")
   }
    
   def gananciaPromedioPorTransporte(viajes: Set[Viaje]) = {
-    impresionEstadisticasPorTransporte(viajes, filtrarPorTipoDeTransporte,gananciaPromedioDeViajes,"GANANCIA PROMEDIO DE VIAJE","Ganancia($)")
+    EstadisticasPorTransporte(viajes,gananciaPromedioDeViajes,"GANANCIA PROMEDIO DE VIAJE","Ganancia($)")
   }
-   
-  def viajesPorTipoTransporte(viajes: Set[Viaje]) ={
+  
+  def viajesPorTipoTransporte(viajes: Set[Viaje]) = {
+    EstadisticasPorTransporte(viajes,cantidadDeViajes,"VIAJES POR TRANSPORTE","Cantidad")
+  }
+  
+  def EstadisticasPorEnvio(viajes: Set[Viaje], f2: Set[Viaje] => _, titulo: String, subt: String) ={
+    
+    val viajesNormal : Set[Viaje] = filtrarPorTipoDeEnvio(viajes, "Normal")
+    val viajesUrgente : Set[Viaje] = filtrarPorTipoDeEnvio(viajes, "Urgente")
+    val viajesRefrigeracion : Set[Viaje] = filtrarPorTipoDeEnvio(viajes, "Refrigeracion")
+    val viajesFragil : Set[Viaje] = filtrarPorTipoDeEnvio(viajes, "Fragil")
+    
+    println(titulo)
+    println("--------------------------")
+    println("Envio   -    " + subt)
+    println("Normal       :    " + f2(viajesNormal))
+    println("Urgente    :    " + f2(viajesUrgente))
+    println("Refrigeracion        :    " + f2(viajesRefrigeracion))
+    println("Fragil        :    " + f2(viajesFragil))
+    println("--------------------------\n")
+    val tuplaNormal: Tupla=new Tupla("camion", f2(viajesNormal))
+    val tuplaUrgente: Tupla=new Tupla("furgoneta", f2(viajesUrgente))
+    val tuplaRefrigeracion: Tupla=new Tupla("avion", f2(viajesRefrigeracion))
+    val tuplaFragil: Tupla=new Tupla("avion", f2(viajesFragil))
+    var datos:Set[Tupla]=Set()
+    //datos += tuplaNormal + tuplaUrgente + tuplaRefrigeracion + tuplaFragil
+    
+  }
+  
+  // Comparacion entre distintas Sucursales
+  def EstadisticasPorSucursal(sucursales : Set[Sucursal],viajes: Set[Viaje], f2: Set[Viaje] => _) ={
+    sucursales.foreach({s =>
+		f2(s.viajesRealizados)
+    })
+  }
+  /*def viajesPorTipoTransporte(viajes: Set[Viaje]) ={
     val viajesCamion : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Camion")
     val viajesFurgoneta : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Furgoneta")
     val viajesAvion : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Avion")
@@ -113,7 +149,7 @@ class Estadisticas extends prettyPrinter{
     println("Furgoneta    :    " + viajesFurgoneta.size)
     println("Avion        :    " + viajesAvion.size)
     println("--------------------------\n")
-  }
+  }*/ //Reemplazado con metodos que implementan orden superior
   
   /*def enviosPorTipoTransporte(viajes: Set[Viaje]) ={
     val viajesCamion : Set[Viaje] = filtrarPorTipoDeTransporte(viajes, "Camion")
